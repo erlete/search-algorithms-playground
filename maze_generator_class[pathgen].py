62e0c08a01a48ddbe911d26bd1340ea91155d896
Paulo Sanchez <szblzpaulo@gmail.com>
2021-04-20 15:43:28 +0200

    Overall description enhancements, method optimization
    Revised docstrings and comments. Added some visual representation for surrounding nodes getter methods. Optimized said methods, as well as the debug logging ones.

--- maze_generator_class[pathgen].py
+++ maze_generator_class[pathgen].py
@@ -2,12 +2,14 @@ from random import randint
 from sys import path
 
 class Maze:
+    """
+    This class generates a blank array of (n x m) dimensions, composed by '-1' values in the borders and '0' values in the middle, the 'Base' property.
 
-    def __init__(self):
-        with open(path[0] + "/textual_log.txt", 'w') as f:  # Resets the textual log file
-            f.write('\n')
-        with open(path[0] + "/visual_log.txt", 'w') as f:   # Resets the visual log file
-            f.write('\n')
+    Given that array, an algorithm generates a 'path', made out of '1' values, which later on can be evaluated by a search algorithm to keep track of the explored path, whose value is '2'.
+
+    There are some debugging methods, such as 'log' and 'logdisplay', that are used in order to properly comprehend the path generation and solving behavior, and also help prevent potential bugs.
+    """
+    # Tool method
 
     def uniqueappend(self, element, group):
         """
@@ -15,6 +17,8 @@ class Maze:
         """
         if element not in group:
             group.append(element)
+
+    # Visualization method
 
     def display(self):
         """
@@ -37,20 +41,29 @@ class Maze:
                     printed_row += 'B ' # Goal state display
             print(printed_row)
 
-    def log(self, item: str, switch = True):
+    # Debugging methods
+
+    def log(self, item: str, switch = False):
         """
-        Logging method for debugging markers in the algorithm.
+        Logging method for convenient data markers in the algorithm.
         """
         if switch == True:
-            with open(path[0] + "/textual_log.txt", 'a') as f:
+            try:
+                f = open(path[0] + "/textual_log.txt", 'r')
+            except FileNotFoundError:
+                f = open(path[0] + "/textual_log.txt", 'a')
                 f.write(item + '\n')
+                f.close()
 
-    def logdisplay(self, switch = True):
+    def logdisplay(self, switch = False):
         """
         Logging method for 'Base' property. Same behavior as the 'display' method.
         """
         if switch == True:
-            with open(path[0] + "/visual_log.txt", 'a') as f:
+            try:
+                f = open(path[0] + "/visual_log.txt", 'r')
+            except FileNotFoundError:
+                f = open(path[0] + "/visual_log.txt", 'a')
                 for row in self.Base:
                     printed_row = ''
                     for element in row:
@@ -73,6 +86,10 @@ class Maze:
                     f.write(str(row)[1:-1].replace(',', ''))
                 f.write('\n\n')
 
+                f.close()
+
+    # Base generation method
+
     def basegenerator(self, dimensions = 10): # review (remove outer borders) (check for surrounding and sqsurrounding nodes behavior)
         """
         Generates a numerical array, given its code structure (-1 = outer wall; 0 = inner wall; 1 = path; 'A' = initial state).
@@ -85,108 +102,102 @@ class Maze:
         elif type(dimensions) == tuple and len(dimensions) == 2:
             for value in dimensions:
                 if value <= 0:
-                    raise Exception("Negative values cannot be used to set the array dimensions")
+                    raise Exception("Negative values cannot be used to set the array dimensions.")
+                elif value > 2:
+                    raise Exception("The array can only be bidimensional.")
         else:
-            raise Exception("The array dimensions must be specified either as an 'int' or two-term 'tuple'")
+            raise Exception("The array dimensions must be specified either as an 'int' or two-term 'tuple'.")
 
-        self.Base = [[0 for i in range(dimensions[0] + 2)] for j in range(dimensions[1] + 2)] # base generation (inner and outer walls only)
+        # Base generation
+        self.Base = [[0 for i in range(dimensions[0] + 2)] for j in range(dimensions[1] + 2)] # Base generation (0)
 
-        self.Base[0] = [-1 for i in range(len(self.Base[0]))]   # top outer wall
+        self.Base[0] = [-1 for i in range(len(self.Base[0]))]   # Top outer wall (-1)
+        self.Base[-1] = self.Base[0].copy()                     # Bottom outer wall (-1)
         for i in range(len(self.Base)):
-                self.Base[i][0] = -1                            # left outer wall
-                self.Base[i][-1] = -1                           # right outer wall
-        self.Base[-1] = [-1 for i in range(len(self.Base[0]))]  # bottom outer wall
+                self.Base[i][-1] = -1                           # Right outer wall (-1)
+                self.Base[i][0] = -1                            # Left outer wall (-1)
 
-        self.Base[1][1] = 'A' # initial state
-        self.Initial_position = (1, 1)
+        self.Base[1][1] = 'A'           # Initial state definition
+        self.Initial_position = (1, 1)  # Initial state position
+
+    # Node surroundings evaluation methods
 
     def nextnodes(self, coordinates):
         """
-        Gets the nodes next to the given coordinates (top, right, bottom and left).
+        Gets the nodes next to the given coordinates (top, right, bottom and left slots).
 
         The order in which the surrounding nodes are passed to the algorithm is set randomly, to prevent data pre-setting.
-        """
-        # Generates an order array:
-        order = []
-        while True:
-            value = randint(0, 3)
-            self.uniqueappend(value, order)
-            if len(order) == 4:
-                break
+        This is done by getting the coordinates of the nodes surrounding the one specified and appending those coordinates to an output array in a randomly defined order.
 
-        # Gets the nodes surrounding the one specified:
+        Note: there must be a more efficient alternative to this process, however, this is not a priority just yet.
+        """
+        # Process illustration:
+
+            #       T
+            #   L   C   R
+            #       B
+
+        # Gets the nodes surrounding the one specified
         surrounding_nodes = []
         for node in [
-                (coordinates[0] - 1, coordinates[1]),   # top
-                (coordinates[0], coordinates[1] + 1),   # right
-                (coordinates[0] + 1, coordinates[1]),   # bottom
-                (coordinates[0], coordinates[1] - 1)    # left
+                (coordinates[0] - 1, coordinates[1]),   # Top
+                (coordinates[0], coordinates[1] + 1),   # Right
+                (coordinates[0] + 1, coordinates[1]),   # Bottom
+                (coordinates[0], coordinates[1] - 1)    # Left
                 ]:
-            try:
-                surrounding_nodes.append(node)
-            except IndexError:
-                pass
+            surrounding_nodes.append(node)
 
-        # Outputs the surrounding nodes in the randomly defined order:
-        output = []
-        while True:
-            for value in order:
-                self.uniqueappend(surrounding_nodes[value], output)
-            if len(output) == 4:
+        # Generates an order array
+        order = []
+        while True: # The infinite loop is necessary in order to provide with unique, random index appending to the 'order' array
+            value = randint(0, len(surrounding_nodes) - 1)
+            self.uniqueappend(value, order) # If a value is repeated, it does not get appended
+            if len(order) == len(surrounding_nodes):
                 break
+
+        # Outputs the surrounding nodes in the randomly defined order
+        output = []
+        for value in order:
+            self.uniqueappend(surrounding_nodes[value], output)
 
         return output
 
     def surroundings(self, coordinates):
         """
         Gets the values in the square surroundings of the given coordinates. This method is used in order to prevent path mixing during its generation.
+
+        Since the method is only used to evaluate the amount of nearby 'path' values (1) near the considered node during path generation, there is no point in setting up a random order method.
         """
+        # Process illustration:
+
+            #   TL  TC  TR
+            #   ML  CC  MR
+            #   BL  BC  BR
+
+        # Gets the nodes surrounding the one specified
         surrounding_nodes = []
         for node in [
-            (coordinates[0] - 1, coordinates[1] - 1),   # top left
-            (coordinates[0] - 1, coordinates[1]),       # top center
-            (coordinates[0] - 1, coordinates[1] + 1),   # top right
-            (coordinates[0], coordinates[1] - 1),       # left
-            (coordinates[0], coordinates[1] + 1),       # right
-            (coordinates[0] + 1, coordinates[1] - 1),   # bottom left
-            (coordinates[0] + 1, coordinates[1]),       # bottom center
-            (coordinates[0] + 1, coordinates[1] + 1)    # bottom right
+            (coordinates[0] - 1, coordinates[1] - 1),   # Top left
+            (coordinates[0] - 1, coordinates[1]),       # Top center
+            (coordinates[0] - 1, coordinates[1] + 1),   # Top right
+            (coordinates[0], coordinates[1] + 1),       # Middle right
+            (coordinates[0] + 1, coordinates[1] + 1),   # Bottom right
+            (coordinates[0] + 1, coordinates[1]),       # Bottom center
+            (coordinates[0] + 1, coordinates[1] - 1),   # Bottom left
+            (coordinates[0], coordinates[1] - 1)       # Middle left
             ]:
-            try:
-                surrounding_nodes.append(node)
-            except IndexError:
-                pass
+            surrounding_nodes.append(node)
+
         return surrounding_nodes
-
-    def distance(self, item_1: tuple, item_2: tuple): # deprecated, fix abs() # review
-        """
-        Returns the diagonal distance between two nodes.
-        """
-        return format(((item_2[0] - item_1[0]) ** 2 + (item_2[1] - item_1[1]) ** 2) ** (1 / 2), '.4f')
-
-    def manhattan(self, item_1: tuple, item_2: tuple):
-        """
-        Returns the manhattan (square) distance between two nodes.
-        """
-        return (abs(item_2[0]) - abs(item_1[0])) + (abs(item_2[1]) - abs(item_1[1]))
-
-    def wdistance(self, item: tuple): # review
-        """
-        Returns the weighted manhattan (square) distance between two nodes.
-        """
-        return (
-            (self.Goal_position[0] - item[0]) + (self.Goal_position[1] - item[1])
-            ), (
-                (item[0] - self.Initial_position[0]) + (item[1] - self.Initial_position[1])
-                )
 
     def caesar(self, paths, spread_index):
         """
-        Random path divergence generator. Takes one or multiple path divergence possibilities and selects some of them.
+        Random path divergence generator. Takes one or multiple path divergence possibilities and selects at least one of them.
 
-        The 'spread_index' sets the probability of path selection ( = 0: no paths are skipped; = 10: all paths are skipped). Note that values near 10 might generate an endless loop.
+        The 'spread_index' sets the probability of path selection ( = 0: all possible paths are selected; = 10: all possible paths are skipped).
+        Note that values near 10 might generate an endless loop. Also, negative values act the same way as zero.
 
-        The name is due to the 'lives, dies' choice of Julius Caesar during collosseum games. # review
+        The name is due to the 'lives, dies' choice of Julius Caesar during colosseum gladiator games.
         """
         # Error Prevention:
         if len(paths) == 0:
@@ -208,18 +219,17 @@ class Maze:
         """
         Sets the position of the goal state at the farthest coordinate possible in the array.
         """
-        for i in range(len(self.Base)):
-            for j in range(len(self.Base[i])):
-                if self.Base[i][j] == 'A':
-                    initial_position = (i, j)
+        # Sets a default higher distance
+        higher_distance = (self.Initial_position[0] + nodes_changed[0][0], self.Initial_position[1] + nodes_changed[0][1])
 
-        total = (initial_position[0] + nodes_changed[0][0], initial_position[1] + nodes_changed[0][1])
+        # Checks every path node's distance to origin to determine which one is the farthest one
+        for node in nodes_changed:
+            if (self.Initial_position[0] + node[0], self.Initial_position[1] + node[1]) >= higher_distance:
+                higher_distance = (self.Initial_position[0] + node[0], self.Initial_position[1] + node[1])
 
-        for i in range(len(nodes_changed)):
-            if (initial_position[0] + nodes_changed[i][0], initial_position[1] + nodes_changed[i][1]) > total:
-                total = (initial_position[0] + nodes_changed[i][0], initial_position[1] + nodes_changed[i][1])
+        return (higher_distance[0] - self.Initial_position[0], higher_distance[1] - self.Initial_position[1])
 
-        return (total[0] - initial_position[0], total[1] - initial_position[1])
+    # Path generation method
 
     def pathgenerator(self, iterations = 200, spread_index = 3):
         """
@@ -237,6 +247,8 @@ class Maze:
                 for candidate_in_range in self.nextnodes(coordinate):
                     if self.Base[candidate_in_range[0]][candidate_in_range[1]] in [-1, 'A', 1]:
                         continue
+                    elif self.Base[candidate_in_range[0]][candidate_in_range[1]] == 'B':
+                        keep = False
                     self.uniqueappend(candidate_in_range, self.Nodes_in_range)
 
             for candidate in self.Nodes_in_range:
@@ -267,6 +279,8 @@ class Maze:
             self.Nodes_in_range = []
             self.Frontier = []
 
+    # Miscellaneous
+
     def quickgen(self, dimensions = 10, iterations = 200, spread_index = 3):
         """
         Quick array and path generation with result display.
@@ -276,6 +290,6 @@ class Maze:
         self.display()
 
 maze = Maze()
-maze.basegenerator(10)
+maze.basegenerator()
 maze.pathgenerator()
 maze.display()
