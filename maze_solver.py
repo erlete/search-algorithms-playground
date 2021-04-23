af0424f21342fd307ef9db8712afcf5f01cd134f
Paulo Sanchez <szblzpaulo@gmail.com>
2021-04-23 18:37:00 +0200

    Solve Method Update #2
    Added second pathfinding method. This one selects a node from the frontier depending on its distance to the goal node. Picks the closest one and evaluates it.
    
    Fixed some minor performance issues regarding comparison checks such as 'if node not in [explored, self.Initial, self.Goal]', which did not check if the node was in the 'explored' array, only 'if node == explored', which returned False every time, since 'type(node)' = 'tuple' and 'type(explored)' = 'list'.

--- maze_solver.py
+++ maze_solver.py
@@ -8,6 +8,19 @@ class Solver():
         self.Initial, self.Goal = obj.Initial, obj.Goal
         self.nextnodes, self.surroundings, self.display = obj.nextnodes, obj.surroundings, obj.display
 
+        self.Distances = dict()
+        for row in range(len(self.Base)):
+            for column in range(len(self.Base[row])):
+                if self.Base[row][column] == 1:
+                    self.Distances[(row, column)] = self.manhattan((row, column))
+
+    def manhattan(self, coordinates: tuple):
+        """
+        Returns the manhattan distance between two nodes (sum of the absolute cartesian coordinates difference between a selected node and the goal node).
+        The values are set as absolute to prevent search deviations due to negative coordinates addition.
+        """
+        return abs(self.Goal[0] - coordinates[0]) + abs(self.Goal[1] - coordinates[1])
+
     def log(self, item, switch):
         if switch:
             print(item)
@@ -32,24 +45,68 @@ class Solver():
                 frontier = frontier[:-1]
 
                 # 2. Node conversion to path
-                if node not in [explored, self.Initial, self.Goal]:
+                if node not in [self.Initial, self.Goal]:
                     self.Base[node[0]][node[1]] = 2
-                    explored.append(node)
+                    explored.append(node) if node not in explored else None
 
                     # 3. Neighbor gathering and pathfinding process termination evaluation
                     for neighbor in self.nextnodes(node):
-                        if self.Base[neighbor[0]][neighbor[1]] == 1 and neighbor not in [explored, frontier]:
-                            frontier.append(neighbor)
+                        if self.Base[neighbor[0]][neighbor[1]] == 1:
+                            frontier.append(neighbor) if neighbor not in frontier else None
                         elif self.Base[neighbor[0]][neighbor[1]] == 10:
                             te = time()
-                            print(f"Array modified correctly. Time elapsed: {format(te - ts, '.4f')}s")
+                            print(f"Array searched correctly. Time elapsed: {format(te - ts, '.4f')}s.")
                             return 1
             
             # If there are no nodes to be explored (no solution)
             else:
                 return 0
 
-maze = Maze(); maze.basegenerator((20, 60)); maze.pathgenerator()
-maze.display()
+    def solve_2(self):
+        """
+        Greedy Best-First Search (GBFS).
+        """
+        global explored, frontier
 
-solve = Solver(maze); solve.solve_1(); solve.display()
+        ts = time()
+
+        frontier, explored = list(), [self.Initial]
+        for node in self.nextnodes(self.Initial):
+            if self.Base[node[0]][node[1]] == 1:
+                frontier.append(node)
+
+        while True:
+
+            # If there are nodes to be explored (avaliable solution)
+            if len(frontier) >= 1:
+
+                # 1. Node extraction from the frontier
+                node = frontier[-1]
+                for candidate in frontier: # Takes the node with the lower distance to goal
+                    if self.Distances[(candidate[0], candidate[1])] < self.Distances[(node[0], node[1])]:
+                        node = candidate
+                del frontier[frontier.index(node)]
+
+                # 2. Node conversion to path
+                if node not in [self.Initial, self.Goal]:
+                    self.Base[node[0]][node[1]] = 2
+                    explored.append(node) if node not in explored else None
+
+                    # 3. Neighbor gathering and pathfinding process termination evaluation
+                    for neighbor in self.nextnodes(node):
+                        if self.Base[neighbor[0]][neighbor[1]] == 1:
+                            frontier.append(neighbor) if neighbor not in frontier else None
+                        elif self.Base[neighbor[0]][neighbor[1]] == 10:
+                            te = time()
+                            print(f"Array searched correctly. Time elapsed: {format(te - ts, '.4f')}s.")
+                            return 1
+
+            # If there are no nodes to be explored (no solution)
+            else:
+                return 0
+
+maze = Maze()
+maze.basegenerator((30, 60)); maze.pathgenerator(); maze.display()
+
+solve = Solver(maze)
+solve.solve_2(); solve.display()
