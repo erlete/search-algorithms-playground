a3366047508371455df56d3e8c5afb54a9f9862c
Paulo Sanchez <szblzpaulo@gmail.com>
2021-06-27 11:00:32 +0200

    Update main.py
    Refactored main according to PEP8's indications.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+"""Main generator and solver module for search algorithms testing."""
+
 from random import randint, sample
 from time import time
+
 
 class Maze:
     """This class generates a blank array of (m x n) dimensions, made of '0'
@@ -12,27 +15,36 @@ class Maze:
     The initial and goal states are represented by '-10' and '10' values,
     respectively.
     """
-    # Base generation method
-
-    def __init__(self, dimensions = 10):
-        if type(dimensions) == int and dimensions > 1:
+    def __init__(self, dimensions=10):
+        if isinstance(dimensions, int) and dimensions > 1:
             self.Dimensions = (dimensions, dimensions)
-        elif type(dimensions) == tuple and len(dimensions) == 2 and dimensions[0] > 1 and dimensions[1] > 1:
+        elif (isinstance(dimensions, tuple) and len(dimensions) == 2
+                and dimensions[0] > 1 and dimensions[1] > 1):
             self.Dimensions = (dimensions[0], dimensions[1])
         else:
-            raise Exception("Dimensions must be either an int or a 2-tuple of ints greater than '1'.")
+            raise Exception(
+                "Dimensions must be either an 'int' or a 2-tuple of 'int's \
+                greater than '1'."
+                )
 
-        self.Base = [[0 for col in range(self.Dimensions[1])] for row in range(self.Dimensions[0])]
-        self.Base[0][0] = -10 # Initial state.
-        self.Initial = (0, 0) # Initial position.
+        self.Base = [
+            [0 for col in range(self.Dimensions[1])]
+            for row in range(self.Dimensions[0])
+            ]
+
+        self.Base[0][0] = -10  # Initial state.
+        self.Initial = (0, 0)  # Initial position.
+        self.Goal = (0, 0)  # Final position
+
+        self.Distances = {}  # Distances tracking for solving methods.
 
     def nextnodes(self, coordinates):
         """Gets the nodes immediately next to the given coordinates from
         'self.Base'.
 
-        The order in which the surrounding nodes are returned is set in a random
-        way, in order to prevent data pre-setting.
-        
+        The order in which the surrounding nodes are returned is set in a
+        random way, in order to prevent data pre-setting.
+
         Process illustration:
         ---------------------
 
@@ -45,8 +57,12 @@ class Maze:
             (coordinates[0], coordinates[1] + 1),   # Right
             (coordinates[0] + 1, coordinates[1]),   # Bottom
             (coordinates[0], coordinates[1] - 1)    # Left
-        ]
-        nextNodes = [node for node in nodes if 0 <= node[0] <= self.Dimensions[0] - 1 and  0 <= node[1] <= self.Dimensions[1] - 1]
+            ]
+        nextNodes = [
+            node for node in nodes
+            if 0 <= node[0] <= self.Dimensions[0] - 1
+            and 0 <= node[1] <= self.Dimensions[1] - 1
+            ]
         return sample(nextNodes, len(nextNodes))
 
     def surroundings(self, coordinates):
@@ -56,7 +72,7 @@ class Maze:
         Since the method is only used to evaluate the amount of nearby 'path'
         values near the considered node during path generation, there is no
         point in returning a randomized sample.
-        
+
         Process illustration:
         ---------------------
 
@@ -73,8 +89,12 @@ class Maze:
             (coordinates[0] + 1, coordinates[1]),       # Bottom center
             (coordinates[0] + 1, coordinates[1] - 1),   # Bottom left
             (coordinates[0], coordinates[1] - 1)        # Middle left
-        ]
-        return [node for node in nodes if 0 <= node[0] <= self.Dimensions[0] - 1 and  0 <= node[1] <= self.Dimensions[1] - 1]
+            ]
+        return [
+            node for node in nodes
+            if 0 <= node[0] <= self.Dimensions[0] - 1
+            and 0 <= node[1] <= self.Dimensions[1] - 1
+            ]
 
     def caesar(self, nodes):
         """Random path divergence generator. Takes one or multiple path
@@ -89,10 +109,15 @@ class Maze:
         """Sets the position of the goal state at the farthest possible
         coordinate in the array.
         """
-        distance = (self.Initial[0] + path_tiles[0][0], self.Initial[1] + path_tiles[0][1])
+        distance = (
+            self.Initial[0] + path_tiles[0][0],
+            self.Initial[1] + path_tiles[0][1]
+            )
         for path in path_tiles:
-            if (self.Initial[0] + path[0], self.Initial[1] + path[1]) >= distance:
-                distance = (self.Initial[0] + path[0], self.Initial[1] + path[1])
+            if ((self.Initial[0] + path[0], self.Initial[1] + path[1])
+                    >= distance):
+                distance = (self.Initial[0] + path[0],
+                            self.Initial[1] + path[1])
         return (distance[0] - self.Initial[0], distance[1] - self.Initial[1])
 
     def pathgenerator(self):
@@ -105,47 +130,54 @@ class Maze:
         while True:
             selectedNodes, candidates = [], []
 
-            # Checks for new path candidates around the node whose values are not '1' (path) nor '-10' (initial node)
             for node in frontier:
-                candidates.extend([neighbor for neighbor in self.nextnodes(node) if self.Base[neighbor[0]][neighbor[1]] not in [-10, 1]])
+                candidates.extend(
+                    [neighbor for neighbor in self.nextnodes(node)
+                        if self.Base[neighbor[0]][neighbor[1]] not in [-10, 1]]
+                    )
             candidates = list(set(candidates))
-            
-            # The candidates are evaluated regarding surrounding path tiles
+
             for candidate in candidates:
-                nearbyPathTiles = len([node for node in self.surroundings(candidate) if self.Base[node[0]][node[1]] in [1, -10]])
+                nearbyPathTiles = len(
+                    [node for node in self.surroundings(candidate)
+                        if self.Base[node[0]][node[1]] in [1, -10]]
+                    )
                 avaliablePath = True if nearbyPathTiles < 3 else False
                 selectedNodes.append(candidate) if avaliablePath else None
 
-            # Out of the candidates' array, only a random sample of them is passed (prevents pre-setting of the data)
             selectedNodes = self.caesar(selectedNodes)
 
-            # Check for the end of the path generation process
-            if selectedNodes == []: # Generation process finished
+            if selectedNodes == []:
                 self.Goal = self.goalspreader(frontier)
                 self.Base[self.Goal[0]][self.Goal[1]] = 10
 
                 te = time()
-                print(f' Array generated correctly. Time elapsed: {(te - ts):.4}s. '.center(self.Dimensions[1] * 2 + 2, '-'))
+                print(
+                    f' Array generated correctly. Time elapsed: \
+                    {(te - ts):.4}s. '.center(self.Dimensions[1] * 2 + 2, '-')
+                    )
 
-                return
-            else: # Generation process not yet finished (there are avaliable nodes for path-making)
-                frontier.extend(selectedNodes)
-                for coordinates in selectedNodes:
-                    self.Base[coordinates[0]][coordinates[1]] = 1
+                return None
+
+            frontier.extend(selectedNodes)
+            for coordinates in selectedNodes:
+                self.Base[coordinates[0]][coordinates[1]] = 1
 
         self.Distances = dict()
         for row in range(len(self.Base)):
             for column in range(len(self.Base[row])):
                 if self.Base[row][column] == 1:
-                    self.Distances[(row, column)] = self.manhattan((row, column))
+                    self.Distances[(row, column)] \
+                        = self.manhattan((row, column))
 
     def manhattan(self, coordinates: tuple):
-        """Returns the manhattan distance between two nodes (sum of the absolute
-        cartesian coordinates difference between a selected node and the goal
-        node).The values are set as absolute to prevent search deviations due to
-        negative coordinates addition.
+        """Returns the manhattan distance between two nodes (sum of the
+        absolute cartesian coordinates difference between a selected node and
+        the goal node).The values are set as absolute to prevent search
+        deviations due to negative coordinates addition.
         """
-        return abs(self.Goal[0] - coordinates[0]) + abs(self.Goal[1] - coordinates[1])
+        return abs(self.Goal[0] - coordinates[0]) \
+            + abs(self.Goal[1] - coordinates[1])
 
     def dfs(self):
         """Depth-First Search (DFS)."""
@@ -153,7 +185,8 @@ class Maze:
         for row in range(len(self.Base)):
             for column in range(len(self.Base[row])):
                 if self.Base[row][column] == 1:
-                    self.Distances[(row, column)] = self.manhattan((row, column))
+                    self.Distances[(row, column)] \
+                        = self.manhattan((row, column))
 
         ts = time()
         print(' Searching array... '.center(self.Dimensions[1] * 2 + 2, '-'))
@@ -162,28 +195,28 @@ class Maze:
 
         while True:
 
-            # If there are nodes to be explored (avaliable solution)
             if len(frontier) >= 1:
-                
-                # 1. Node extraction from the frontier
                 node = frontier[-1]
                 frontier = frontier[:-1]
 
-                # 2. Node conversion to path
                 if node not in [self.Initial, self.Goal]:
                     self.Base[node[0]][node[1]] = 2
                     explored.append(node) if node not in explored else None
 
-                    # 3. Neighbor gathering and pathfinding process termination evaluation
                     for neighbor in self.nextnodes(node):
                         if self.Base[neighbor[0]][neighbor[1]] == 1:
-                            frontier.append(neighbor) if neighbor not in frontier else None
+                            frontier.append(neighbor) \
+                                if neighbor not in frontier else None
                         elif self.Base[neighbor[0]][neighbor[1]] == 10:
                             te = time()
-                            print(f' Array searched correctly. Time elapsed: {(te - ts):.4}s. '.center(self.Dimensions[1] * 2 + 2, '-'))
+                            print(
+                                f' Array searched correctly. Time elapsed: \
+                                {(te - ts):.4}s. '.center(
+                                    self.Dimensions[1] * 2 + 2, '-'
+                                    )
+                                )
                             return 1
-            
-            # If there are no nodes to be explored (no solution)
+
             else:
                 return 0
 
@@ -193,7 +226,8 @@ class Maze:
         for row in range(len(self.Base)):
             for column in range(len(self.Base[row])):
                 if self.Base[row][column] == 1:
-                    self.Distances[(row, column)] = self.manhattan((row, column))
+                    self.Distances[(row, column)] \
+                         = self.manhattan((row, column))
         ts = time()
         print(' Searching array... '.center(self.Dimensions[1] * 2 + 2, '-'))
         frontier, explored = list(), [self.Initial]
@@ -203,49 +237,46 @@ class Maze:
 
         while True:
 
-            # If there are nodes to be explored (avaliable solution)
             if len(frontier) >= 1:
 
-                # 1. Node extraction from the frontier
                 node = frontier[-1]
-                for candidate in frontier: # Takes the node with the lower distance to goal
-                    if self.Distances[(candidate[0], candidate[1])] < self.Distances[(node[0], node[1])]:
+                for candidate in frontier:
+                    if (self.Distances[(candidate[0], candidate[1])]
+                            < self.Distances[(node[0], node[1])]):
                         node = candidate
                 del frontier[frontier.index(node)]
 
-                # 2. Node conversion to path
                 if node not in [self.Initial, self.Goal]:
                     self.Base[node[0]][node[1]] = 2
                     explored.append(node) if node not in explored else None
 
-                    # 3. Neighbor gathering and pathfinding process termination evaluation
                     for neighbor in self.nextnodes(node):
                         if self.Base[neighbor[0]][neighbor[1]] == 1:
-                            frontier.append(neighbor) if neighbor not in frontier else None
+                            frontier.append(neighbor) \
+                                if neighbor not in frontier else None
                         elif self.Base[neighbor[0]][neighbor[1]] == 10:
                             te = time()
-                            print(f' Array searched correctly. Time elapsed: {(te - ts):.4}s. '.center(self.Dimensions[1] * 2 + 2, '-'))
+                            print(
+                                f' Array searched correctly. Time elapsed: \
+                                {(te - ts):.4}s. '.center(
+                                    self.Dimensions[1] * 2 + 2, '-'
+                                    )
+                                )
                             return 1
 
-            # If there are no nodes to be explored (no solution)
             else:
                 return 0
 
     def __repr__(self):
-        visual = {
-            0:   '█ ',
-            1:   '  ',
-            2:   '░ ',
-            3:   '≡ ',
-            -10: 'A ',
-            10:  'B '
-        }
-        return f"╔═{2 * '═' * self.Dimensions[1]}╗\n" +\
-               ''.join(
-                   ''.join(
-                       [f'║ ' + ''.join(
-                           [visual[element] for element in row]
-                       ) + f'║\n']
-                   ) for row in self.Base
-               ) +\
-               f"╚═{2 * '═' * self.Dimensions[1]}╝"
+        visual = {0: '█ ', 1: '  ', 2: '░ ',
+                  3: '≡ ', -10: 'A ', 10: 'B '}
+        return (
+            f"╔═{2 * '═' * self.Dimensions[1]}╗\n"
+            + ''.join(
+                ''.join(
+                    ['║ ' + ''.join(
+                        [visual[element] for element in row]
+                    ) + '║\n']
+                ) for row in self.Base
+            ) + f"╚═{2 * '═' * self.Dimensions[1]}╝"
+            )
